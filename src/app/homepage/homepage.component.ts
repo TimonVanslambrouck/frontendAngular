efd10571a5acc0726cc0228e3660c11388e3aaba
Timon Vanslambrouck <timon.vanslambrouck@student.ehb.be>
2021-08-03 16:57:42 +0200

    finished homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -12,6 +12,16 @@ export class HomepageComponent implements OnInit {
   public productList!: Observable<IProduct[]>
   constructor(private dataService: DataService) { }
 
+  onSave(id:number){
+    let src = document.getElementById(`heart${id}`)?.getAttribute('src');
+    if (src?.includes('blue')){
+      document.getElementById(`heart${id}`)?.setAttribute('src', ('assets/svg/heart-thin.svg'));
+    } else {
+      document.getElementById(`heart${id}`)?.setAttribute('src', ('assets/svg/heart-thin-blue.svg'));
+
+    }
+  }
+
   ngOnInit(): void {
     this.productList = this.dataService.getAll();
   }
