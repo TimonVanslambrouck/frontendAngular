aa9f804b075286ed2585466f1a8febadccf1e463
Timon Vanslambrouck <timon.vanslambrouck@student.ehb.be>
2021-08-04 15:55:05 +0200

    productlist page done

--- src/app/productlistpage/productlistpage.component.ts
+++ src/app/productlistpage/productlistpage.component.ts
@@ -1,4 +1,7 @@
+import { IProduct } from './../data';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-productlistpage',
@@ -11,9 +14,42 @@ export class ProductlistpageComponent implements OnInit {
   showPrice = false;
   showReview = false;
   showColor = false;
-  constructor() { }
+  productListSorted:any;
+  public productList!: Observable<IProduct[]>
+  constructor(private dataService: DataService) {
+    this.dataService.getAll()
+    .subscribe(
+      (data) => {
+        for (const product of data) {
+          if (this.productListSorted == undefined) {
+            this.productListSorted = [product];
+          } else {
+            this.productListSorted.push(product);
+          }         
+        }
+      }
+    )
+   }
+
+  addToCart(id:number){
+    // add to cart
+  }
 
   ngOnInit(): void {
+    this.productList = this.dataService.getAll();
+    this.dataService.getAll()
+    .subscribe(
+      (data) => {
+        for (const product of data) {
+          if (this.productListSorted == undefined) {
+            this.productListSorted = [product];
+          } else {
+            this.productListSorted.push(product);
+          }         
+        }
+        
+      }
+    )
   }
 
 }
